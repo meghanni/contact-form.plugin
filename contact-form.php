b46e0285559bb3395fc3a2df7fa6f8851a61d348
Meghan Nicholas <meghan.nicholas@gmail.com>
2013-04-18 21:03:11 +0100

    changed location of class.view

--- contact-form.php
+++ contact-form.php
@@ -33,7 +33,7 @@ include('class.cff_pluginsettings.php');
 include('class.cff_settings.php');
 include('class.cff_contact.php');
 
-include('mvc/class.view.php');
+include('class.view.php');
 
 if ( cff_PluginSettings::UseRecaptcha() )
     include('recaptcha-php-1.11/recaptchalib.php');
